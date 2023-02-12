17b67ab63574c73356ad6bc9c944a70e543db95c
X512 <danger_mail@list.ru>
2023-02-12 23:04:51 +0900

    threadlink: update
    Change-Id: I63a2e396003f9b6e15e60a6f329d965beec768a8

--- src/libs/threadlink/ClientThreadLink.cpp
+++ src/libs/threadlink/ClientThreadLink.cpp
@@ -1,62 +1,75 @@
 #include "ClientThreadLink.h"
+#include <PthreadMutexLocker.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <new>
 
 
-thread_local ObjectDeleter<ClientThreadLink> tlsClientThreadLink = NULL;
-
-
-ClientThreadLink::ClientThreadLink(port_id serverPort):
-	fPort(create_port(100, "client"))
+ClientThreadLink::ClientThreadLink(ClientThreadLinkConnection *conn, const BMessenger &serverMsgr):
+	fConn(conn), fSender(B_ERROR), fReceiver(B_ERROR), fPort(create_port(100, "client"))
 {
-	printf("+ThreadLink(), thread: %" B_PRId32 "\n", find_thread(NULL));
-	fLink.SetTo(serverPort, fPort.Get());
-	int32 replyCode;
-	port_id serverThreadPort;
-	fLink.StartMessage(connectMsg);
-	fLink.Attach<int32>(fPort.Get());
-	fLink.FlushWithReply(replyCode);
-	fLink.Read<int32>(&serverThreadPort);
-	fLink.SetTo(serverThreadPort, fPort.Get());
-}
+	fprintf(stderr, "+ThreadLink(), thread: %" B_PRId32 "\n", find_thread(NULL));
 
-ClientThreadLink::ClientThreadLink(const BMessenger &serverMsgr):
-	fPort(create_port(100, "client"))
-{
-	printf("+ThreadLink(), thread: %" B_PRId32 "\n", find_thread(NULL));
-	fLink.SetTo(-1, fPort.Get());
-	int32 replyCode;
-	port_id serverThreadPort;
 	BMessage msg(connectMsg);
-	msg.AddInt32("port", fPort.Get()); 
+	msg.AddInt32("port", fPort.Get());
 	serverMsgr.SendMessage(&msg);
-	fLink.GetNextMessage(replyCode);
-	fLink.Read<int32>(&serverThreadPort);
-	fLink.SetTo(serverThreadPort, fPort.Get());
+
+	int32 replyCode;
+	port_id serverThreadPort;
+	fReceiver.SetPort(fPort.Get());
+	fReceiver.GetNextMessage(replyCode);
+	fReceiver.Read(&serverThreadPort);
+	fSender.SetPort(serverThreadPort);
 }
 
 ClientThreadLink::~ClientThreadLink()
 {
-	printf("-ThreadLink(), thread: %" B_PRId32 "\n", find_thread(NULL));
-	fLink.StartMessage(disconnectMsg);
-	fLink.Flush();
+	fprintf(stderr, "-ThreadLink(), thread: %" B_PRId32 "\n", find_thread(NULL));
+	fSender.StartMessage(disconnectMsg);
+	fSender.Flush();
+	PthreadMutexLocker lock(&fConn->fLock);
+	fConn->fLinks.Remove(this);
 }
 
 
-ClientThreadLink *GetClientThreadLink(port_id serverPort)
+ClientThreadLinkConnection::ClientThreadLinkConnection()
 {
-	if (!tlsClientThreadLink.IsSet()) {
-		ClientThreadLink *threadLink = new ClientThreadLink(serverPort);
-		tlsClientThreadLink.SetTo(threadLink);
-	}
-	return tlsClientThreadLink.Get();
+	if (pthread_key_create(&fLinkTls, [](void *arg) {
+		delete static_cast<ClientThreadLink*>(arg);
+	}) < 0) abort();
 }
 
-ClientThreadLink *GetClientThreadLink(const BMessenger &serverMsgr)
+ClientThreadLinkConnection::~ClientThreadLinkConnection()
 {
-	if (!tlsClientThreadLink.IsSet()) {
-		ClientThreadLink *threadLink = new ClientThreadLink(serverMsgr);
-		tlsClientThreadLink.SetTo(threadLink);
+	PthreadMutexLocker lock(&fLock);
+	while (ClientThreadLink *threadLink = fLinks.First()) {
+		delete threadLink;
 	}
-	return tlsClientThreadLink.Get();
+	if (pthread_key_delete(fLinkTls) < 0) abort();
+}
+
+void ClientThreadLinkConnection::SetMessenger(const BMessenger &serverMsgr)
+{
+	fServerMsgr = serverMsgr;
+}
+
+
+ThreadLinkHolder::ThreadLinkHolder(ClientThreadLinkConnection &conn)
+{
+	ClientThreadLink *threadLink = (ClientThreadLink*)pthread_getspecific(conn.fLinkTls);
+	if (threadLink == NULL) {
+		PthreadMutexLocker lock(&conn.fLock);
+		threadLink = new ClientThreadLink(&conn, conn.fServerMsgr);
+		conn.fLinks.Insert(threadLink);
+		pthread_setspecific(conn.fLinkTls, threadLink);
+	}
+	fSender = &threadLink->fSender;
+	fReceiver = &threadLink->fReceiver;
+}
+
+ThreadLinkHolder::~ThreadLinkHolder()
+{
+	CancelMessage();
 }
