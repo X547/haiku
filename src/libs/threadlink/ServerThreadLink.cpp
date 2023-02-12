37661fd95292ac57789818e3cf4f630f2407b243
X512 <danger_mail@list.ru>
2023-02-12 23:04:51 +0900

    threadlink: update
    Change-Id: I63a2e396003f9b6e15e60a6f329d965beec768a8

--- src/libs/threadlink/ServerThreadLink.cpp
+++ src/libs/threadlink/ServerThreadLink.cpp
@@ -1,36 +1,41 @@
 #include "ServerThreadLink.h"
 #include <stdio.h>
 
+static void Check(status_t res) {if (res < B_OK) abort();}
 
-thread_local ServerThreadLink *tlsServerThreadLink = NULL;
 
-
-ServerThreadLink::ServerThreadLink(port_id clientPort):
-	fPort(create_port(100, "server"))
+ServerThreadLink::ServerThreadLink(port_id clientPort)
 {
+	fPort.SetTo(create_port(100, "server"));
+	Check(fPort.Get());
+
 	port_info portInfo;
-	get_port_info(clientPort, &portInfo);
+	Check(get_port_info(clientPort, &portInfo));
 	fClientTeam = portInfo.team;
 
 	fLink.SetTo(clientPort, fPort.Get());
+}
 
-	fLink.StartMessage(B_OK);
-	fLink.Attach<int32>(fPort.Get());
-	fLink.Flush();
+ServerThreadLink::~ServerThreadLink()
+{
+	fprintf(stderr, "-ServerThreadLink(), thread: %" B_PRId32 ", team: %" B_PRId32 "\n", fThread, fClientTeam);
+}
 
+void ServerThreadLink::Start()
+{
 	fThread = spawn_thread(
 		[] (void *arg) {return ((ServerThreadLink*)arg)->ThreadEntry();},
 		"client thread",
 		B_NORMAL_PRIORITY,
 		this
 	);
-	printf("+ServerThreadLink(), thread: %" B_PRId32 ", team: %" B_PRId32 "\n", fThread, fClientTeam);
+	fprintf(stderr, "+ServerThreadLink(), thread: %" B_PRId32 ", team: %" B_PRId32 "\n", fThread, fClientTeam);
+	Check(fThread);
 	resume_thread(fThread);
-}
 
-ServerThreadLink::~ServerThreadLink()
-{
-	printf("-ServerThreadLink(), thread: %" B_PRId32 ", team: %" B_PRId32 "\n", fThread, fClientTeam);
+	fLink.StartMessage(B_OK);
+	fLink.Attach<int32>(fPort.Get());
+	fLink.Flush();
 }
 
 void ServerThreadLink::Close()
@@ -38,13 +43,15 @@ void ServerThreadLink::Close()
 	BPrivate::PortLink link(fPort.Get(), -1);
 	link.StartMessage(disconnectMsg);
 	link.Flush();
+	close_port(fPort.Get());
 }
 
 void ServerThreadLink::MessageReceived(int32 what)
 {
 	(void)what;
 	if (fLink.NeedsReply()) {
-		fLink.StartMessage(B_ERROR);
+		fprintf(stderr, "[!] ServerThreadLink: unhandled message: %" B_PRId32 ", thread: %" B_PRId32 ", team: %" B_PRId32 "\n", what, fThread, fClientTeam);
+		fLink.StartMessage(ENOSYS);
 		fLink.Flush();
 	}
 }
@@ -54,7 +61,7 @@ status_t ServerThreadLink::ThreadEntry()
 	for (;;) {
 		int32 what;
 		fLink.GetNextMessage(what);
-		//printf("thread message received: %" B_PRId32 "\n", what);
+		//fprintf(stderr, "thread message received: %" B_PRId32 "\n", what);
 		switch (what) {
 			case disconnectMsg:
 				if (fLink.NeedsReply()) {
@@ -69,43 +76,4 @@ status_t ServerThreadLink::ThreadEntry()
 		}
 	}
 	return B_OK;
-}
-
-
-ServerThreadLink *GetServerThreadLink()
-{
-	return tlsServerThreadLink;
-}
-
-
-ServerLinkWatcher::ServerLinkWatcher(port_id serverPort, ServerThreadLink *(*Factory)(port_id clientPort)):
-	fLink(-1, serverPort),
-	fFactory(Factory)
-{
-}
-
-void ServerLinkWatcher::Quit()
-{
-	printf("[!] ServerLinkWatcher::Quit: not implemented\n");
-	abort();
-}
-
-void ServerLinkWatcher::Run()
-{
-	for (;;) {
-		int32 what;
-		fLink.GetNextMessage(what);
-		switch (what) {
-			case quitServerMsg: {
-				return;
-			}
-			case connectMsg: {
-				port_id replyPort;
-				fLink.Read<int32>(&replyPort);
-				BPrivate::LinkSender replySender(replyPort);
-				fFactory(replyPort);
-				break;
-			}
-		}
-	}
 }
