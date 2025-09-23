3c562aac1945b7409805184126edff92cba57558
X512 <danger_mail@list.ru>
2025-09-23 11:54:49 +0900

    app_server: write terminator string for AS_GET_SYSTEM_FONTS
    Change-Id: I5f5b0d04bb9526e731d90372dcf45d5c24b42605

--- src/servers/app/ServerApp.cpp
+++ src/servers/app/ServerApp.cpp
@@ -1900,6 +1900,7 @@ ServerApp::_DispatchMessage(int32 code, BPrivate::LinkReceiver& link)
 				fLink.Attach<uint16>(font->Face());
 				fLink.Attach<uint32>(font->Flags());
 			}
+			fLink.AttachString(""); // terminator
 
 			fDesktop->UnlockSingleWindow();
 			fLink.Flush();
